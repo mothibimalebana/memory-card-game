41f88958679a293da4574de4ca076bc6c607bbcf
mothibimalebana <mothibi.mm12@gmail.com>
2025-01-31 15:12:03 +0200

    Changed applictaion title to Memory Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() =>{
+    fetch('https://pokeapi.co/api/v2/pokemon/').then((res) => {
+      return console.log(res)
+    })
+  },[characters])
+}
 
+function App() {
   return (
     <>
+    <Characters/>
     </>
   )
 }
