c5572b8b1fa7a09794fa69345081f89c3ad1660f
mothibimalebana <mothibi.mm12@gmail.com>
2025-01-31 15:33:13 +0200

    fetch is now responding with 12 items instead of 20

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,15 @@ const Characters = () => {
   const [characters, setCharacters] = useState([]);
   useEffect(() =>{
     fetch('https://pokeapi.co/api/v2/pokemon/').then((res) => {
-      return console.log(res)
+      return res.json()
+    })
+    .then((res) => {
+      res.results.filter((item, index) => {
+        index < 12 ? console.log(item) : null
+      })
+    })
+    .catch((res) => {
+      console.log('something went wrong')
     })
   },[characters])
 }
