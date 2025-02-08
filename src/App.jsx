2d7209f679fb7d3a53511e08e73faab93ca33419
mothibimalebana <mothibi.mm12@gmail.com>
2025-02-08 21:06:14 +0200

    Created: Card.jsx & Card.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-const Characters = () => {
-  const [characters, setCharacters] = useState([]);
-  useEffect(() =>{
-    fetch('https://pokeapi.co/api/v2/pokemon/').then((res) => {
-      return res.json()
-    })
-    .then((res) => {
-      res.results.filter((item, index) => {
-        index < 12 ? console.log(item) : null
-      })
-    })
-    .catch((res) => {
-      console.log('something went wrong')
-    })
-  },[characters])
-}
+import './App.css'
 
 function App() {
   return (
     <>
-    <Characters/>
     </>
   )
 }
