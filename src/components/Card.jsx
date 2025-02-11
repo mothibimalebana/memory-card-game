d1f31faa1e9b3f3a60013da488f94ba5cb241b2d
mothibimalebana <mothibi.mm12@gmail.com>
2025-02-11 22:53:33 +0200

    fetch now gets the image as well as index of each item
    before it would be hard to statically link image of each card to it's right place, fetching the characters along with their position is easier compared to the previous method used

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
 
 // CardItems Component: Displays each character with the sprite image and name
-function CardItems({ character, itemNO, shuffleCharacters, addPoint }) {
+function CardItems({ character, itemNO, shuffleCharacters, addPoint, playerPoints, setPlayerPoints }) {
     const [wasClicked, setWasClicked] = useState(false);
   
     function addPoint(){
-
+      setPlayerPoints(playerPoints => playerPoints + 1)
+      console.log(playerPoints)
     }
     function handleClick(){
         shuffleCharacters();
-
+        addPoint()
         setWasClicked(!wasClicked);
     }
     // Construct the sprite image URL using itemNO
@@ -29,6 +30,7 @@ function CardItems({ character, itemNO, shuffleCharacters, addPoint }) {
 // Cards Component: Fetches the Pokémon data and renders cards
 function Cards() {
   const [characters, setCharacters] = useState([]);
+  const [playerPoints, setPlayerPoints] = useState(1);
 
   function shuffleCharacters(){
     const sortedArr = structuredClone(characters);
@@ -36,7 +38,7 @@ function Cards() {
         let j = Math.floor(Math.random() * (i + 1));
         [sortedArr[i], sortedArr[j]] = [sortedArr[j], sortedArr[i]]
     }
-    return sortedArr;
+    setCharacters(sortedArr);
   }
 
   useEffect(() => {
@@ -45,6 +47,10 @@ function Cards() {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
         const data = await response.json();
         const resCharacters = data.results.slice(0, 12); // Get first 12 entries
+        const resCharactersIndex = resCharacters.map((item, index) => index + 1)
+        console.log(resCharacters)
+        console.log(resCharactersIndex)
+
         setCharacters(resCharacters);
       } catch (error) {
         console.error('Error fetching characters:', error);
@@ -57,7 +63,7 @@ function Cards() {
   return (
     <div className="cards-container">
       {characters.map((character, index) => (
-        <CardItems shuffleCharacters={shuffleCharacters} character={character} key={character.name} itemNO={index} />
+        <CardItems setPlayerPoints={setPlayerPoints} playerPoints={playerPoints} shuffleCharacters={shuffleCharacters} character={character} key={character.name} itemNO={index} />
       ))}
     </div>
   );
